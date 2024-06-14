f742ef25abfc78182c15a23f04de0a65928a882d
DxVxDOD <orbangdavid@yahoo.com>
2024-06-14 20:20:00 +0300

    nodemon working with esm modules

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,4 +1,4 @@
-import express, { type Response } from "express";
+import express, { type Response, type Request } from "express";
 import cors from "cors";
 
 const app = express();
@@ -7,8 +7,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("dist"));
 
-app.use("/", (res: Response) => res.status(200).json("pong"));
+app.use("/", (_req: Request, res: Response) => res.status(200).json("pong"));
 
 app.get("/*", (_req, res) => {
   res.sendFile("/index.html", { root: "./dist" });
 });
+
+export default app;
